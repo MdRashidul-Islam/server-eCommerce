51131bf389783f2f0bf8fd5d60a4ef973b4a89d6
rashidul rony <rashidulrony79775@gmail.com>
2021-12-19 22:30:54 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient } = require("mongodb");
 const admin = require("firebase-admin");
 const ObjectId = require("mongodb").ObjectId;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
-const fileUpload = require("express-fileUpload");
+const fileUpload = require("express-fileupload");
 
 const port = process.env.PORT || 5000;
 
